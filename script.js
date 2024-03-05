eb76b85a96d47001fd8b00987d76ed70423d9cb9
CedricFournie <cf11157701@gmail.com>
2024-03-05 20:03:30 +0100

    ajout des frais

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const apy = document.getElementById("apy");
 const bcalculer = document.getElementById("bcalculer");
 const pgainannuel = document.getElementById("pgainannuel");
 const pgainmensuel = document.getElementById("pgainmensuel");
+const frais = document.getElementById("frais");
 
 bcalculer.addEventListener("click", () => {
     const npool = pool.value;
     const napy = apy.value / 100;
-    const aresult =  npool * napy
+    const cfrais = npool * napy / 365
+    const aresult =  npool * napy - cfrais
     const mresult =  aresult / 12
     pgainannuel.innerHTML = aresult + "€"
     pgainmensuel.innerHTML = mresult + "€"
