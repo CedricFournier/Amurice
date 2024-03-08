a2eb1947b97b1dd3d30fd0918f28f1ef53856ebe
CedricFournie <cf11157701@gmail.com>
2024-03-09 00:15:25 +0100

    fin calcul composer

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const bcalculer = document.getElementById("bcalculer");
 const pgainannuel = document.getElementById("pgainannuel");
 const pgainmensuel = document.getElementById("pgainmensuel");
 const tabdetail = document.querySelector(".tabdetail");
+const tabcompound = document.querySelector(".tabcompound");
 
 bcalculer.addEventListener("click", () => {
     const npool = pool.value;
@@ -13,10 +14,11 @@ bcalculer.addEventListener("click", () => {
     const mresult =  aresult / 12
     pgainannuel.innerHTML = aresult + "€"
     pgainmensuel.innerHTML = mresult + "€"
-    detail(npool, mresult)
+    detail(mresult)
+    detailcompound(npool, napy)
 });
 
-function detail (npool, mresult) {
+function detail (mresult) {
     let i = 1;
     for (i ; i < 13; i++) {
         const tableau =  
@@ -27,5 +29,24 @@ function detail (npool, mresult) {
             </tr>
         ` 
         tabdetail.insertAdjacentHTML("beforeend", tableau);
+    };
+}
+
+function detailcompound (npool, napy) {
+    const mtaux = napy / 12
+    const jtaux = napy / 365
+    const taux = mtaux - jtaux
+    let i = 0;
+    for (i ; i < 12; i++) {
+        const tableau =  
+        `
+            <tr>
+                <td>${i + 1}</td>
+                <td>${npool * taux * Math.pow(1 + (mtaux - jtaux), i) + " €"}</td>
+                <td>${npool * Math.pow(1 + (mtaux - jtaux), i + 1) - npool + " €"}</td>
+                <td>${npool * Math.pow(1 + (mtaux - jtaux), i + 1) + " €"}</td>
+            </tr>
+        ` 
+        tabcompound.insertAdjacentHTML("beforeend", tableau);
     };
 }
