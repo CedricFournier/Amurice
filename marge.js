e75f775da8593b3a4e027b13728e5a69cedb310e
CedricFournie <cf11157701@gmail.com>
2024-03-18 14:22:04 +0100

    ajout btn reset

--- marge.js
+++ marge.js
@@ -2,12 +2,10 @@ const pachat = document.getElementById("pachat");
 const pvente = document.getElementById("pvente");
 const quantite = document.getElementById("quantite");
 const ctransport = document.getElementById("ctransport");
-const conditionnement = document.getElementById("conditionnement");
 const tcommercial = document.getElementById("tcommercial");
-const errorchamps = document.getElementById("errorchamps");
+const breset = document.getElementById("breset");
 const bcalculer = document.getElementById("bcalculer");
 const chiffreaffaire = document.getElementById("chiffreaffaire");
-const poids = document.getElementById("poids");
 const coutachat = document.getElementById("coutachat");
 const margebrut = document.getElementById("margebrut");
 const impot = document.getElementById("impot");
@@ -17,18 +15,25 @@ const margenet = document.getElementById("margenet");
 bcalculer.addEventListener("click", () => {
     const caffaire = pvente.value * quantite.value;
     const cachat = pachat.value * quantite.value;
-    const cpoids = conditionnement.value * quantite.value;
     const mbrut = caffaire - cachat;
-    const cimpot = caffaire * 0.123;
+    const cimpot = caffaire * 0.13;
     const ccommercial = caffaire * tcommercial.value / 100;
     const mnet = mbrut - cimpot - ccommercial - ctransport.value;
 
-    chiffreaffaire.innerHTML = caffaire + "€";
-    poids.innerHTML = cpoids + "KG"
-    coutachat.innerHTML = cachat + "€";
-    margebrut.innerHTML = mbrut + "€";
-    impot.innerHTML = cimpot + "€";
-    pcommercial.innerHTML = ccommercial + "€";
-    margenet.innerHTML = mnet + "€";
+    chiffreaffaire.innerHTML = caffaire.toFixed(2) + "€";
+    coutachat.innerHTML = cachat.toFixed(2) + "€";
+    margebrut.innerHTML = mbrut.toFixed(2) + "€";
+    impot.innerHTML = cimpot.toFixed(2) + "€";
+    pcommercial.innerHTML = ccommercial.toFixed(2) + "€";
+    margenet.innerHTML = mnet.toFixed(2) + "€";
     
 });
+
+breset.addEventListener("click", () => {
+    pachat.value = "";
+    pvente.value = "";
+    quantite.value = "";
+    ctransport.value = "";
+    tcommercial.value = "";
+
+})
